b8d416db86da370d26a874b70da39bc6b70a9f67
cy19960119 <CYcz1996>
2018-02-26 15:18:33 +0800

    first commit

---
+++ page/cocktail/cocktail.js
@@ -0,0 +1,103 @@
+// page/cocktail/cocktail.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    goods:{
+      id:1,
+      image:'images/like.png',
+      price:100,
+      title:'蓝色夏威夷',
+      stock:'有货',
+      num:1,
+      totalNum:0,
+      hasCarts:false,
+      scaleCart:false
+    }
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  },
+
+
+  /**
+   * 自定义函数
+  */
+  addToCard(){
+    const self = this;
+    const total = this.data.totalNum;
+
+    self.setData({
+      show:true
+    })
+    setTimeout(function () {
+      self.setData({
+        show: false,
+        scaleCart: true
+      })
+      setTimeout(function () {
+        self.setData({
+          scaleCart: false,
+          hasCarts: true,
+          totalNum: num + total
+        })
+      }, 200)
+    }, 300)
+  }
+})
