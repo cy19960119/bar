abeb97a6da1db8c54cbf1361a1b2b08ab85856b6
cy19960119 <CYcz1996>
2018-02-28 14:05:41 +0800

    修改

--- page/cocktail/cocktail.js
+++ page/cocktail/cocktail.js
@@ -11,10 +11,10 @@ Page({
       price:100,
       title:'蓝色夏威夷',
       stock:'有货',
-      num:1,
-      totalNum:0,
-      hasCarts:false,
-      scaleCart:false
+      // num:1,
+      // totalNum:0,
+      // hasCarts:false,
+      // scaleCart:false
     }
 
   },
@@ -79,25 +79,70 @@ Page({
   /**
    * 自定义函数
   */
-  addToCard(){
-    const self = this;
-    const total = this.data.totalNum;
+  addToCard:function(e){
+    // const self = this;
+    // const total = this.data.totalNum;
 
-    self.setData({
-      show:true
-    })
-    setTimeout(function () {
-      self.setData({
-        show: false,
-        scaleCart: true
-      })
-      setTimeout(function () {
-        self.setData({
-          scaleCart: false,
-          hasCarts: true,
-          totalNum: num + total
-        })
-      }, 200)
-    }, 300)
+    // self.setData({
+    //   show:true
+    // })
+    // setTimeout(function () {
+    //   self.setData({
+    //     show: false,
+    //     scaleCart: true
+    //   })
+    //   setTimeout(function () {
+    //     self.setData({
+    //       scaleCart: false,
+    //       hasCarts: true,
+    //       totalNum: num + total
+    //     })
+    //   }, 200)
+    // }, 300)
+    this.setData({
+      toastHidden:false
+    });
+    //遍历列表和购物车列表
+    for(var i in this.data.goods){
+      //列表中某一项item的id == 点击事件传来的id。则是被点击的项
+      if(this.data.goods[i].id == e.target.id){
+        //给goods数组的当前项添加count元素，值为1，用于记录添加到购物车的数量
+        this.data.goods[i].count = 1;
+        //获取购物车缓存数组（没有数据，则赋予一个空数组）
+        var arr = wx.getStorageSync('cart') || [];
+        if(arr.length>0){
+          //遍历购物车数组
+          for(var j  in arr){
+            //判断购物车内的item的id,和事件传递过来的id，是否相等
+            if(arr[j].id == e.target.id){
+              //相等的话，给count加1
+              arr[j].count = arr[j].count + 1;
+              //最后把购物车数据，存放入缓存（此处不用再给购物车数组push元素进去，因为这个是购物车有的，直接更新当前数组）
+              try{
+                wx.setStorageSync('cart', arr)
+              }catch(e){
+                console.log(e)
+              }
+              //返回（在if内使用return，跳出循环节约运算，节约性能）
+              return;
+            }
+          }
+          // 遍历完购物车后，没有对应的item项，把goods的当前项放入购物车数组  
+          arr.push(this.data.goods[i]);  
+        }
+        // 购物车没有数据，把item项push放入当前数据（第一次存放时）  
+        else {
+          arr.push(this.data.goods[i]);
+        }   
+        // 最后，把购物车数据，存放入缓存  
+        try {
+          wx.setStorageSync('cart', arr)
+          // 返回（在if内使用return，跳出循环节约运算，节约性能）  
+          return;
+        } catch (e) {
+          console.log(e)
+        }  
+      }
+    }
   }
 })
--- page/products/products.js
+++ page/products/products.js
@@ -16,6 +16,16 @@ Page({
    */
 
   data: {
+    category:[
+      { name: '鸡尾酒', id: 1, image:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1519638618257&di=235813c9609b2977c3dc954733954704&imgtype=jpg&src=http%3A%2F%2Fimg2.imgtn.bdimg.com%2Fit%2Fu%3D2942062407%2C3079893499%26fm%3D214%26gp%3D0.jpg'},
+      { name: '啤酒', id: 2, image:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1519622259525&di=1078f254a729e0f2fa613082a62a5b37&imgtype=0&src=http%3A%2F%2Fpic2.16pic.com%2F00%2F12%2F78%2F16pic_1278181_b.jpg'},
+      {name:'洋酒',id:3,image:''},
+      {name:'酒桶',id:4,image:''},
+      {name:'软饮',id:5,image:''},
+      {name:'水烟',id:6,image:''},
+      {name:'小吃',id:7,image:''}
+    ],
+
   },
 
   /**
